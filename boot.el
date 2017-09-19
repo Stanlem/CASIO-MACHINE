9ce429220d098a954147473055588f6e49acff4c
ThinkPad <rodrigoffvh@gmail.com>
2017-09-19 02:04:53 -0500

    changes

--- boot.el
+++ boot.el
@@ -1,33 +1,36 @@
 
+(require 'org)
+;; (if (window-system)
+;;     (split-window-right (floor (* 0.85 (window-width)))))
+
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
 
-(add-hook 'lisp-interaction-mode-hook 'electric-pair-mode)
-(add-hook 'emacs-lisp-mode-hook 'electric-pair-mode)
-(add-hook 'lisp-mode-hook 'electric-pair-mode)
-(add-hook 'lisp-mode-hook 'sly-mode)
 
 (keyboard-translate ?\( ?\[)
 (keyboard-translate ?\[ ?\()
 (keyboard-translate ?\) ?\])
 (keyboard-translate ?\] ?\))
 
-(defun lisp ()
-  (interactive)
-  (split-window-horizontally)
-  (find-file "~/.emacs.d/casio.cl")
-  (sly))
+(toggle-frame-fullscreen)
 
-(defalias 'yes-or-no-p 'y-or-n-p)
-(if (window-system)
-      (split-window-right (floor (* 0.85 (window-width)))))
+(add-hook 'lisp-interaction-mode-hook 'slime-mode)
+(add-hook 'lisp-interaction-mode-hook 'electric-pair-mode)
+(add-hook 'emacs-lisp-mode-hook 'electric-pair-mode)
+(add-hook 'lisp-mode-hook 'electric-pair-mode)
+(add-hook 'lisp-mode-hook 'slime-mode)
 
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
+(setq Tex-auto-save t)
+(setq TeX-parse-self t)
+(require 'tex-mik) 
+(load "auctex.el" nil t t)
 
-(global-set-key (kbd "M-p") 'ace-window)
+(setq org-export-with-LaTeX-fragments "dvipng")
+(setq org-return-follows-link t)
+
+(add-hook 'sgml-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook  'emmet-mode)
 
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
@@ -35,26 +38,20 @@
 ;; Old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
-(add-hook 'sgml-mode-hook 'emmet-mode)
-(add-hook 'css-mode-hook  'emmet-mode)
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
 
-(require 'org)
-
-;; (setq slime-lisp-implementations '((sbcl ("C:\\Program Files\\Steel
-;; Bank Common Lisp\\1.3.18\\sbcl.exe" "--core" "C:\\Program Files\\Steel
-;; Bank Common Lisp\\1.3.18\\sbcl.core"))))
-
-;; (add-hook 'lisp-interaction-mode-hook 'electric-pair-mode)
-;; (add-hook 'lisp-interaction-mode-hook 'sly-mode)
-
-;; (setq Tex-auto-save t)
-;; (setq TeX-parse-self t)
-;; (require 'tex-mik) 
-;; (load "auctex.el" nil t t)
-;; (setq org-export-with-LaTeX-fragments "dvipng")
-;; (setq org-return-follows-link t)
+(defalias 'yes-or-no-p 'y-or-n-p)
+(global-set-key (kbd "M-p") 'ace-window)
 
 (defun halt ()
   (interactive)
   (save-some-buffers)
   (kill-emacs))
+
+(defun lisp ()
+  (interactive)
+  (split-window-horizontally)
+  (find-file "~/.emacs.d/casio.cl")
+  (sly))
